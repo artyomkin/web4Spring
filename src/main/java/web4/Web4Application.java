7d2af0bb17320b2ee89989c731eff82e1f058871
Artyomkin Artyom <welllet@mail.ru>
2022-01-24 19:33:21 +0300

    initial commit

--- src/main/java/web4/Web4Application.java
+++ src/main/java/web4/Web4Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication /*start of the program*/
-public class Web4Application {
+public class  Web4Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Web4Application.class, args);
